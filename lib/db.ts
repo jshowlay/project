321a63b7911268e4c6fb91960fa83fbee3186784
Justin <jshowlay@gmail.com>
2026-01-11 17:05:41 -0800

    fix: Add QueryResultRow import to lib/db.ts
    - Add QueryResultRow to imports from pg
    - Fixes TypeScript error: Cannot find name 'QueryResultRow'

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { Pool, PoolClient, QueryResult } from 'pg';
+import { Pool, PoolClient, QueryResult, QueryResultRow } from 'pg';
 import { logger } from './logger';
 
 // Database connection pool
